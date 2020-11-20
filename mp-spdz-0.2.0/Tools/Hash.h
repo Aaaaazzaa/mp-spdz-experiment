c111af786d6dd72d92c163487affb3165a94200e
Leihao Chen <Aaaaazzaa@github.com>
2020-11-20 07:19:09 -0600

    init commit

---
+++ mp-spdz-0.2.0/Tools/Hash.h
@@ -0,0 +1,54 @@
+#ifndef _SHA1
+#define _SHA1
+
+#include <sodium.h>
+#include <vector>
+using namespace std;
+
+#include "octetStream.h"
+
+class Hash
+{
+	crypto_generichash_state* state;
+
+public:
+	static const int hash_length = crypto_generichash_BYTES;
+
+	unsigned size;
+
+	Hash();
+	~Hash();
+
+	void reset();
+
+	void update(const void *dataIn, unsigned long len)
+	{
+		crypto_generichash_update(state, (unsigned char*)dataIn, len);
+		size += len;
+	}
+	void update(const octetStream& os);
+	template<class T>
+	void update(const T& x)
+	{
+	    update(x.get_ptr(), x.size());
+	}
+	template<class T>
+	void update(const vector<T>& v)
+	{
+	    octetStream tmp(v.size() * sizeof(T));
+	    for (auto& x : v)
+	        x.pack(tmp);
+	    update(tmp);
+	}
+
+	void final(unsigned char hashout[hash_length])
+	{
+		crypto_generichash_final(state, hashout, crypto_generichash_BYTES);
+	}
+	void final(octetStream& os);
+	octetStream final();
+};
+
+void hash_update(Hash *ctx, const void *dataIn, unsigned long len);
+
+#endif
